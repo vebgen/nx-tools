c2de834339f064e1c0911f1a25410fd1097a5820
Nicu Tofan <nicu.tofan@gmail.com>
2024-01-17 18:53:41 +0200

    Added a generator

---
+++ exploration/src/generators/mygen/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nx/devkit';
+
+import { mygenGenerator } from './generator';
+import { MygenGeneratorSchema } from './schema';
+
+describe('mygen generator', () => {
+  let tree: Tree;
+  const options: MygenGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await mygenGenerator(tree, options);
+    const config = readProjectConfiguration(tree, 'test');
+    expect(config).toBeDefined();
+  });
+});
